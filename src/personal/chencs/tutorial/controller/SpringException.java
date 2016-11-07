70f813f353c9919f77d998f5f898d4dc5e345a83
chencs0603 <1021823177@qq.com>
2016-11-07 15:05:53 +0800

    Spring异常处理

---
+++ src/personal/chencs/tutorial/controller/SpringException.java
@@ -0,0 +1,19 @@
+package personal.chencs.tutorial.controller;
+
+public class SpringException extends RuntimeException {
+
+	private String exceptionMsg;
+
+	public SpringException(String exceptionMsg) {
+		this.exceptionMsg = exceptionMsg;
+	}
+
+	public String getExceptionMsg() {
+		return exceptionMsg;
+	}
+
+	public void setExceptionMsg(String exceptionMsg) {
+		this.exceptionMsg = exceptionMsg;
+	}
+	
+}
--- src/personal/chencs/tutorial/controller/StudentController.java
+++ src/personal/chencs/tutorial/controller/StudentController.java
@@ -2,6 +2,7 @@ package personal.chencs.tutorial.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,10 +19,20 @@ public class StudentController {
 	}
 	
 	@RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+	@ExceptionHandler({SpringException.class})
 	public String addStudent(@ModelAttribute("SpringWeb")Student student, ModelMap model){
 		model.addAttribute("id", student.getId());
-		model.addAttribute("name", student.getName());
-		model.addAttribute("age", student.getAge());
+		if(student.getName().length() < 5){
+			throw new SpringException("Given name is too short");
+		}else{
+			model.addAttribute("name", student.getName());
+		}
+		
+		if(student.getAge() < 10){
+			throw new SpringException("Given age is too low");
+		}else{
+			model.addAttribute("age", student.getAge());
+		}
 		
 		return "result";
 	}
