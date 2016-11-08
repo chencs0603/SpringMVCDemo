ef082e25ab024e9bfbf83e7ae4d0547525d8a149
chencs0603 <1021823177@qq.com>
2016-11-08 15:45:03 +0800

    配置Web应用的首页

--- src/personal/chencs/tutorial/controller/HelloController.java
+++ src/personal/chencs/tutorial/controller/HelloController.java
@@ -3,13 +3,19 @@ package personal.chencs.tutorial.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 
 @Controller
-@RequestMapping("/hello")
 public class HelloController{
+	
+	@RequestMapping(value ="/", method =RequestMethod.GET)
+    public String home(Model model)
+    {        
+        return"index";
+    }
  
-   @RequestMapping(method = RequestMethod.GET)
+   @RequestMapping(value = "/hello", method = RequestMethod.GET)
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");
 
